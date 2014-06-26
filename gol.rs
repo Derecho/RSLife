6089db24129c799b78c0c47972dfb8b90310f590
Derecho <derecho@sector5d.org>
2014-06-26 23:05:48 +0200

    Added basic functions

--- gol.rs
+++ gol.rs
@@ -1,8 +1,20 @@
 // Game of Life implementation in Rust by Derecho.
 
+fn update_screen() {
+    // TODO
+}
+
+fn run(interval: f32) {
+    println!("Running Game of Life with {} fps", 1.0/interval);
+    loop {
+        // TODO Sleep interval
+        update_screen();
+    }
+}
+
 fn main() {
     // Get these from input
     let interval = 0.2;
 
-    println!("Running Game of Life with {} fps", 1.0/interval);
+    run(interval);
 }
