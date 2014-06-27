201c33ab8b49dfecb4e926f89c7af8dd2853019b
Derecho <derecho@sector5d.org>
2014-06-27 23:34:17 +0200

    Implemented generation and printing of grid. Rows are malfunctioning.

--- gol.rs
+++ gol.rs
@@ -1,32 +1,88 @@
 // Game of Life implementation in Rust by Derecho.
+extern crate debug;
 
 use std::io::timer;
+use std::rand::random;
 
 struct Cell {
     alive: bool
 }
 
-fn draw_grid() {
-    // TODO
+struct Grid {
+    cells: Vec<Vec<Cell>>
 }
 
-fn run(interval: f32) {
-    let mut generation = 0;
-    loop {
-        print!("\x1B[2J");  // Clear screen
-        println!("Running Game of Life with {} fps", 1.0/interval);
-        println!("Generation: {}", generation);
+struct Game {
+    grid: Grid
+}
+impl Game {
+    fn draw_grid(&self) {
+        for row in self.grid.cells.iter() {
+            println!("[D] row: {:?}", row);
+            for cell in row.iter() {
+                println!("[D] cell: {:?}", cell);
+                match cell.alive {
+                    false => print!(" "),
+                    true  => print!("X")
+                }
+            }
+            println!("");
+        }
+    }
 
-        draw_grid();
-        
-        generation += 1;
-        timer::sleep((interval * 1000.0) as u64);
+    fn run(&self, interval: f32) {
+        let mut generation = 0;
+        loop {
+            print!("\x1B[2J");  // Clear screen
+            println!("Running Game of Life with {} fps", 1.0/interval);
+            println!("Generation: {}", generation);
+
+            self.draw_grid();
+
+            generation += 1;
+            timer::sleep((interval * 1000.0) as u64);
+        }
+    }
+
+    fn empty_grid(size: int) -> Grid {
+        let mut grid = vec![];
+        let mut i = 0;
+        let mut j = 0;
+        while i < size {
+            let mut row = vec![];
+            while j < size {
+                row.push(Cell { alive: false });
+                j += 1;
+            }
+            grid.push(row);
+            i += 1;
+        };
+        Grid { cells: grid }
+    }
+
+    fn random_grid(size: int) -> Grid {
+        let mut grid = vec![];
+        let mut i = 0;
+        let mut j = 0;
+        while i < size {
+            let mut row = vec![];
+            while j < size {
+                row.push(Cell { alive: random::<bool>() });
+                j += 1;
+            }
+            grid.push(row);
+            i += 1;
+        };
+        Grid { cells: grid }
     }
 }
+
 
 fn main() {
     // Get these from input
     let interval = 0.2;
+    let size = 10;
 
-    run(interval);
+    let game = Game { grid: Game::random_grid(size) };
+    game.run(interval);
 }
