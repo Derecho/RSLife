3ff99594921d4ac2e8b78eb1ea994343c21cbd2f
Derecho <derecho@sector5d.org>
2014-06-28 17:12:04 +0200

    Refactored code, added Cell getter in Grid

--- gol.rs
+++ gol.rs
@@ -8,15 +8,17 @@ struct Cell {
 }
 
 struct Grid {
-    cells: Vec<Vec<Cell>>
+    cells: Vec<Vec<Cell>>,
+    size: uint
 }
+impl Grid {
+    fn get<'r>(&'r self, x: int, y: int) -> &'r Cell {
+        self.cells.get((y % self.size as int) as uint)
+            .get((x % self.size as int) as uint)
+    }
 
-struct Game {
-    grid: Grid
-}
-impl Game {
-    fn draw_grid(&self) {
-        for row in self.grid.cells.iter() {
+    fn draw(&self) {
+        for row in self.cells.iter() {
             for cell in row.iter() {
                 match cell.alive {
                     false => print!(" "),
@@ -27,43 +29,52 @@ impl Game {
         }
     }
 
+    #[allow(dead_code)]
+    fn empty_grid(size: uint) -> Grid {
+        let mut grid = Vec::new();
+        for _ in range(0, size) {
+            let mut row = Vec::new();
+            for _ in range(0, size) {
+                row.push(Cell { alive: false });
+            }
+            grid.push(row);
+        };
+        Grid { cells: grid, size: size }
+    }
+
+    fn random_grid(size: uint) -> Grid {
+        let mut grid = Vec::new();
+        for _ in range(0, size) {
+            let mut row = Vec::new();
+            for _ in range(0, size) {
+                row.push(Cell { alive: random::<bool>() });
+            }
+            grid.push(row);
+        };
+        Grid { cells: grid, size: size }
+    }
+}
+
+struct Game {
+    grid: Grid
+}
+impl Game {
+    fn tick(&self) {
+        // TODO
+    }
+
     fn run(&self, interval: f32) {
         let mut generation = 0;
         loop {
             print!("\x1B[2J");  // Clear screen
             println!("Running Game of Life with {} fps", 1.0/interval);
             println!("Generation: {}", generation);
+            self.grid.draw();
 
-            self.draw_grid();
-
-            generation += 1;
             timer::sleep((interval * 1000.0) as u64);
+            self.tick();
+            generation += 1;
         }
-    }
-
-    #[allow(dead_code)]
-    fn empty_grid(size: int) -> Grid {
-        let mut grid = vec![];
-        for _ in range(0, size) {
-            let mut row = vec![];
-            for _ in range(0, size) {
-                row.push(Cell { alive: false });
-            }
-            grid.push(row);
-        };
-        Grid { cells: grid }
-    }
-
-    fn random_grid(size: int) -> Grid {
-        let mut grid = vec![];
-        for _ in range(0, size) {
-            let mut row = vec![];
-            for _ in range(0, size) {
-                row.push(Cell { alive: random::<bool>() });
-            }
-            grid.push(row);
-        };
-        Grid { cells: grid }
     }
 }
 
@@ -73,6 +84,6 @@ fn main() {
     let interval = 0.2;
     let size = 10;
 
-    let game = Game { grid: Game::random_grid(size) };
+    let game = Game { grid: Grid::random_grid(size)};
     game.run(interval);
 }
